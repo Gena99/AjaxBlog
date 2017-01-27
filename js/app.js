af13825a9658da166f170e4c508c8c78917fea25
Gena <gena@e64.net>
2017-01-27 21:34:00 +0100

    Etape2 : Créer, récupérer et afficher un menu en json dans le document

--- js/app.js
+++ js/app.js
@@ -3,10 +3,20 @@
 	var app = {
 		init:function(){
 			var converter = new showdown.Converter();
+			// Récupérer un texte en MarkDown et l'afficher converti dans le document
 			$.get("example.md", function(data){
 				//console.log(data);
 				var texteMarkDownConvertiEnHTML = converter.makeHtml(data);
     			$('#md').html(texteMarkDownConvertiEnHTML);
+			});
+			
+			// Charger et afficher le menu dans le document
+			$.get("menu.json", function(data){
+				//console.log(data);
+				data.listeMenu.forEach(function(menu, i, listeMenu){
+					//console.log('Resultat iteration', menu);
+					$('#menu ul').append('<li><a href="' + menu.url + '">' + menu.titre + '</a></li>');
+				});
 			});
 		}
 	};
